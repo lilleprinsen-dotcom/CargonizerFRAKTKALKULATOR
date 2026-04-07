7b6db7032af72f30f6fe4918252c1ccfbc5c83fb
lilleprinsen-dotcom <lilleprinsen@gmail.com>
2026-04-07 19:24:24 +0200

    Fix integration test style for PHPCS compliance

--- tests/Integration/WooShippingRegistrationTest.php
+++ tests/Integration/WooShippingRegistrationTest.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * Integration test for WooCommerce shipping method registration.
+ *
+ * @package Lilleprinsen\Cargonizer\Tests\Integration
+ */
 
 namespace Lilleprinsen\Cargonizer\Tests\Integration;
 
@@ -6,17 +11,21 @@ use Lilleprinsen\Cargonizer\Shipping\ShippingMethodRegistry;
 use Lilleprinsen\Cargonizer\Shipping\WooShippingIntegration;
 use PHPUnit\Framework\TestCase;
 
-final class WooShippingRegistrationTest extends TestCase
-{
-    public function testRegisterMethodsMergesRegistryMap(): void
-    {
-        $registry = $this->createMock(ShippingMethodRegistry::class);
-        $registry->method('getMethodClassMap')->willReturn(['lp_cargonizer_1_2' => 'MethodClass']);
+/**
+ * Verifies shipping method registration integration behavior.
+ */
+final class WooShippingRegistrationTest extends TestCase {
+	/**
+	 * Merges dynamic Cargonizer methods with existing WooCommerce methods.
+	 */
+	public function testRegisterMethodsMergesRegistryMap(): void {
+		$registry = $this->createMock( ShippingMethodRegistry::class );
+		$registry->method( 'getMethodClassMap' )->willReturn( [ 'lp_cargonizer_1_2' => 'MethodClass' ] );
 
-        $integration = new WooShippingIntegration($registry);
-        $methods = $integration->registerMethods(['flat_rate' => 'WC_Shipping_Flat_Rate']);
+		$integration = new WooShippingIntegration( $registry );
+		$methods     = $integration->registerMethods( [ 'flat_rate' => 'WC_Shipping_Flat_Rate' ] );
 
-        self::assertSame('MethodClass', $methods['lp_cargonizer_1_2']);
-        self::assertSame('WC_Shipping_Flat_Rate', $methods['flat_rate']);
-    }
+		self::assertSame( 'MethodClass', $methods['lp_cargonizer_1_2'] );
+		self::assertSame( 'WC_Shipping_Flat_Rate', $methods['flat_rate'] );
+	}
 }
